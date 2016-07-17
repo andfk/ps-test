004662fde2c7114aae8204778376ce695b328a75
Andrés F <hello@andresfran.co>
2016-07-17 15:55:49 +0200

    Add improvements to test

--- test/wistia-uploader.spec.js
+++ test/wistia-uploader.spec.js
@@ -8,7 +8,20 @@ describe('wistia-uploader', function(){
 
   describe('directive', function(){
 
-    var $compile, $rootScope, $log;
+    var $compile, $rootScope, $log, templates;
+
+    templates = {
+      'directive-with-api' : '<wistia-uploader api-key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"></wistia-uploader>',
+      'directive-without-api' : '<wistia-uploader></wistia-uploader>',
+      'directive-with-classes' : '<wistia-uploader css-classes="pretty-btn" api-key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"></wistia-uploader>',
+      'directive-with-btn-value' : '<wistia-uploader api-key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" btn-value="Upload now!"></wistia-uploader>'
+    };
+
+    function renderDir(template, scope) {
+      var el = $compile(templates[template])(scope);
+      scope.$digest();
+      return el;
+    }
 
     beforeEach(angular.mock.inject(function(_$compile_, _$rootScope_, _$log_){
       $compile = _$compile_;
@@ -18,8 +31,7 @@ describe('wistia-uploader', function(){
 
 
     it('Should log error if directive is rendered without wistia api key', function(){
-      var element = $compile('<wistia-uploader></wistia-uploader>')($rootScope);
-      $rootScope.$digest();
+      renderDir('directive-without-api', $rootScope);
       expect($log.error.logs[0]).toEqual(['API password for Wistia is required. Please create one by https://wistia.com/doc/account-setup#api_access']);
     });
 
@@ -30,12 +42,25 @@ describe('wistia-uploader', function(){
           template += '<input type="file"><div class="wistia-upload-progress-bar ng-hide" ng-show="isUploading" ng-style="{width:progressbarWidth}" style="width: 0%;">';
           template += '</div></button><div ng-show="hasVideo" class="wistia-player-wrapper ng-hide"></div></div>';
 
-      var element = $compile('<wistia-uploader api-key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"></wistia-uploader>')($rootScope);
+      var element = renderDir('directive-with-api', $rootScope);
       $log.reset();
-      $rootScope.$digest();
       expect($log.assertEmpty).not.toThrow();
       expect(element.html()).toEqual(template);
 
+    });
+
+    it('Should check if classes are attached when set as attr', function(){
+      var el = renderDir('directive-with-classes', $rootScope);
+      expect(el.scope().cssClasses).toEqual('wistia-upload-btn pretty-btn');
+      var btn = el.find('button');
+      expect(btn.attr('class')).toEqual('wistia-upload-btn pretty-btn');
+    });
+
+    it('Should check if btnValue changes when set as attr', function(){
+      var el = renderDir('directive-with-btn-value', $rootScope);
+      expect(el.scope().btnValue).toEqual('Upload now!');
+      var btnValue = el.find('button span');
+      expect(btnValue.text()).toEqual('Upload now!');
     });
 
   });
